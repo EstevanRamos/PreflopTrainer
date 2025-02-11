e9e331d1c505ea043b227ac58bfc346a5c9847a4
RamosEstevan <ramos_estevan@outlook.com>
2025-02-11 12:55:25 -0700

    ffixing branches

--- preflop-trainer/src/routes/+page.svelte
+++ preflop-trainer/src/routes/+page.svelte
@@ -1,2 +1,4 @@
 <h1>This page will be landing page eventually</h1>
+<h2>tryign to fix github
+</h2>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
